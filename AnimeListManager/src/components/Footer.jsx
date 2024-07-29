ee3217047c9270d7a2f645e6493fbf7941ba7c65
ZalaJi07 <harshdeepsinhzalabajarda@gmail.com>
2024-07-29 21:10:05 +0530

    AnimeListManager Project Added!

---
+++ AnimeListManager/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4 text-center">
+      <div className="container mx-auto">
+        <p>&copy; 2024 <span className='text-[#E67E22]'>Anime</span>ListManager. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ AnimeListManager/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="bg-[#2C3E50] px-4 text-[#ECF0F1]">
+            <div className="logo"><img src="/logo.png" alt="logo" className='h-14 md:h-12 m-auto md:m-0' /></div>
+        </div>
+    )
+}
+
+export default Navbar
