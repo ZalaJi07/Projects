af33e946715a6980078afb62bf21153a1108b1bc
ZalaJi07 <harshdeepsinhzalabajarda@gmail.com>
2024-10-01 12:00:28 +0530

    Jikan API added for anime search results.

--- AnimeListManager/src/App.jsx
+++ AnimeListManager/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div onContextMenu={(e) => e.preventDefault()} className="flex flex-col min-h-screen">
         <Navbar />
         {/* <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>    */}
         <Manager />
