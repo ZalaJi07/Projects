4cef4cd92bd96747f058d1e669b6726dd8c98054
ZalaJi07 <harshdeepsinhzalabajarda@gmail.com>
2025-10-29 12:42:28 +0530

    Succesfully saves anime in mongoDB Atlas database.

--- AnimeListManager(With Backend)/Frontend/src/main.jsx
+++ AnimeListManager(With Backend)/Frontend/src/main.jsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import reducers from './reducers'
-import {provider} from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { thunk } from 'redux-thunk'
 
-const store = createStore( reducers, compose(applyMiddleware(thunk))); 
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
