0c502f256874e731aa23dccbd2a474ec6b529dd9
ZalaJi07 <harshdeepsinhzalabajarda@gmail.com>
2025-11-07 16:35:35 +0530

    Minor changes for deploying on Render

--- AnimeListManager(With Backend)/Server/Index.js
+++ AnimeListManager(With Backend)/Server/Index.js
@@ -15,12 +15,21 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/anime", entryRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Anime List Manager is running...");
+});
+
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`✅ Server running on port: ${PORT}`)))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+// mongoose
+//   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+//   .catch((error) => console.log(error.message));
 
 // mongoose.set("useFindAndModify", false);
